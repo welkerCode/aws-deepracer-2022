79df18a4e6823111de1c1686f0c253d26a10059a
Taylor Welker <taylormaxwelker@gmail.com>
2022-07-15 22:52:43 -0700

    Pre-Trials commit

---
+++ aws-reward-function.py
@@ -0,0 +1,97 @@
+# Model to beat: TaylorHailMary Model
+# Fastest time: 14.466 sec
+# Avg time: 15.374
+
+{
+    "all_wheels_on_track": Boolean,        # flag to indicate if the agent is on the track
+    "x": float,                            # agent's x-coordinate in meters
+    "y": float,                            # agent's y-coordinate in meters
+    "closest_objects": [int, int],         # zero-based indices of the two closest objects to the agent's current position of (x, y).
+    "closest_waypoints": [int, int],       # indices of the two nearest waypoints.
+    "distance_from_center": float,         # distance in meters from the track center 
+    "is_crashed": Boolean,                 # Boolean flag to indicate whether the agent has crashed.
+    "is_left_of_center": Boolean,          # Flag to indicate if the agent is on the left side to the track center or not. 
+    "is_offtrack": Boolean,                # Boolean flag to indicate whether the agent has gone off track.
+    "is_reversed": Boolean,                # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
+    "heading": float,                      # agent's yaw in degrees
+    "objects_distance": [float, ],         # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
+    "objects_heading": [float, ],          # list of the objects' headings in degrees between -180 and 180.
+    "objects_left_of_center": [Boolean, ], # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
+    "objects_location": [(float, float),], # list of object locations [(x,y), ...].
+    "objects_speed": [float, ],            # list of the objects' speeds in meters per second.
+    "progress": float,                     # percentage of track completed
+    "speed": float,                        # agent's speed in meters per second (m/s)
+    "steering_angle": float,               # agent's steering angle in degrees
+    "steps": int,                          # number steps completed
+    "track_length": float,                 # track length in meters.
+    "track_width": float,                  # width of the track
+    "waypoints": [(float, float), ]        # list of (x,y) as milestones along the track center
+
+}
+
+{
+    # Most promising variables
+    "is_crashed": Boolean,                 # Boolean flag to indicate whether the agent has crashed.
+    "is_left_of_center": Boolean,          # Flag to indicate if the agent is on the left side to the track center or not
+    "speed": float,                        # agent's speed in meters per second (m/s)
+    "steering_angle": float,               # agent's steering angle in degrees
+}
+
+
+# Ideas:
+# Sharper Turns
+# Longer training times
+# Slower speeds
+# Hug the left side fo the track
+
+
+
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+
+    ABS_STEERING_THRESHOLD = 15 
+    SPEED_THRESHOLD = 0.6
+    SPEED_PENALTY = 0.9
+    STEERING_PENALTY = 0.8
+
+    LEFT_OF_CENTER_REWARD = 0.1
+    
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    is_left_of_center = params['is_left_of_center']
+    abs_steering = abs(params['steering_angle'])
+    speed = params['speed']
+
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+
+    if is_left_of_center:
+        reward = reward + LEFT_OF_CENTER_REWARD  
+
+    # The faster you go, the more reward you have
+    reward += reward + (speed*.1)
+
+    # To encourage the car to stay fast
+    #if speed <= SPEED_THRESHOLD:
+    #    reward *= SPEED_PENALTY 
+
+    # Penalize reward if the car is steering too much
+    if abs_steering > ABS_STEERING_THRESHOLD:
+        reward *= STEERING_PENALTY
+
+    return float(reward)
